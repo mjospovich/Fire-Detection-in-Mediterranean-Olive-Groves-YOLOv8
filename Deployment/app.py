4dcbc76f1901ab720a32dbae2115c6bd889c2c5a
mjospovich <martin.josipovic2@gmail.com>
2024-04-09 23:19:34 +0200

    added title, image and starting structure for app

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,9 +1,47 @@
+# Imports
 import streamlit as st
 
+#* Set the page config
+st.set_page_config(
+    page_title="Fire and Smoke Detection",
+    page_icon="🔥",
+    layout="centered",
+    initial_sidebar_state="expanded"
+    )
+
+#* Render the app
 def app():
+
+    # Title, intro and image
     st.title("Fire and Smoke Detection")
-    st.write("Welcome to the Fire and Smoke Detection App!")
-    st.write("Here you can test our DL model for detecting fire and smoke in images.Please upload an image and click on the 'Predict' button to see the results.")
+    st.write("by Martin Josipović")
+    st.divider()
+    st.image(
+        "Deployment/assets/wildfire.png", # Images at Deployment/assets
+        caption="AI generated image of a wildfire",
+        use_column_width="always",
+        )
+    
+    # Description
+    yolo_link = '<a hrf="https://github.com/ultralytics/ultralytics">YOLOv8</a>'
+    dfire_link = '<a hrf="https://github.com/gaiasd/DFireDataset">D-Fire</a>'
+
+    st.write(
+        f"""<br>
+        Our fire and smoke detection model was built using the {yolo_link} model, 
+        trained on the {dfire_link} dataset and some our own data as well. <br>
+        This app enables you to upload an image of your choice and use our model to detect
+        fire and smoke if present in the image.
+    	<br><br><br>
+        """,
+        unsafe_allow_html=True
+        )
+    
+    # Testing the model
+    st.write("## Test the model")
+
+    
+
 
 
 if __name__ == "__main__":
