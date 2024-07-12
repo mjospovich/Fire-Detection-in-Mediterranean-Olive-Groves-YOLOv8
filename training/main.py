1117ad689188cc636f1942ede38ea0e68896b134
Martin <93053179+mjospovich@users.noreply.github.com>
2024-07-12 13:49:28 +0200

    changed set model to nano

--- training/main.py
+++ training/main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8s.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.yaml")  # train a new model (based on nano model -> change last letter for other models, example yolov8s.yaml for small)
 model.to('cuda') # for gpu training (NVIDIA only)
 
 if __name__ == "__main__":
